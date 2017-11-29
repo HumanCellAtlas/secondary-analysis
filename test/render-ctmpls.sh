c3827bd5a21d84a26adae3c97b13c1d93106d450
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-11-29 14:06:02 -0800

    parameterize volume dir

--- test/render-ctmpls.sh
+++ test/render-ctmpls.sh
@@ -2,9 +2,10 @@
 
 env=$1
 vault_token=$2
+working_dir=${3:-$PWD}
 
 printf "Creating Lira config\n"
-docker run -it --rm -v $PWD:/working -e VAULT_TOKEN=$vault_token \
+docker run -it --rm -v $working_dir:/working -e VAULT_TOKEN=$vault_token \
     -e INPUT_PATH=/working/test/config \
     -e OUT_PATH=/working/test \
     broadinstitute/dsde-toolbox render-templates.sh $env
