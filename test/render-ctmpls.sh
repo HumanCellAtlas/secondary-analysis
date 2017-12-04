7685d2673f65cf90d2568038fe8538cef73843ef
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-12-04 11:54:47 -0500

    fix docker call for rendering ctmpls

--- test/render-ctmpls.sh
+++ test/render-ctmpls.sh
@@ -5,7 +5,7 @@ vault_token=$2
 working_dir=${3:-$PWD}
 
 printf "Creating Lira config\n"
-docker run -it --rm -v $working_dir:/working -e VAULT_TOKEN=$vault_token \
+docker run --rm -v $working_dir:/working -e VAULT_TOKEN=$vault_token \
     -e INPUT_PATH=/working/test/config \
     -e OUT_PATH=/working/test \
     broadinstitute/dsde-toolbox render-templates.sh $env
