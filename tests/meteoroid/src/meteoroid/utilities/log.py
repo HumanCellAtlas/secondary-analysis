90ffe8f1a563bb134beda29c781e9018b688bbc1
Rex Wang <rexwangcc@gmail.com>
2019-06-12 13:52:48 -0400

    Init Test suite, create directories and implement basic Vault and log helpers.

---
+++ tests/meteoroid/src/meteoroid/utilities/log.py
@@ -0,0 +1,65 @@
+from enum import Enum
+import os
+import sys
+import logging
+
+
+# In case this will need to run on Windows systems
+if sys.platform.lower() == "win32":
+    os.system('color')
+
+
+class AvailableColors(Enum):
+    GRAY = 90
+    RED = 91
+    GREEN = 92
+    YELLOW = 93
+    BLUE = 94
+    PURPLE = 95
+    WHITE = 97
+    BLACK = 30
+    DEFAULT = 39
+
+
+STYLE_MAPPING = {
+    'DEBUG': 'GRAY',
+    'INFO': 'DEFAULT',
+    'WARNING': 'YELLOW',
+    'ERROR': 'PURPLE',
+    'CRITICAL': 'RED',
+}
+
+
+def dye_msg_with_color(msg: str, color: str) -> str:
+    """Dye message with color, fall back to default if it fails."""
+    color_code = AvailableColors['DEFAULT'].value
+    try:
+        color_code = AvailableColors[color.upper()].value
+    except KeyError:
+        pass
+    return f"\033[{color_code}m{msg}\033[0m"
+
+
+def dye_logger(
+    logger: logging.Logger, style_mapping: dict = STYLE_MAPPING
+) -> logging.Logger:
+    """Dye a logger with colors according to the style_mapping dict."""
+    try:
+        if logger.dyed:
+            return logger
+    except AttributeError:
+
+        def _dye_logging_func(logging_func, color):
+            def logging_func_wrapper(msg, *args, **kwargs):
+                logging_func(dye_msg_with_color(msg, color), *args, **kwargs)
+
+            return logging_func_wrapper
+
+        for func_name, color_name in style_mapping.items():
+            setattr(
+                logger,
+                func_name.lower(),
+                _dye_logging_func(getattr(logger, func_name.lower()), color_name),
+            )
+        setattr(logger, 'dyed', True)
+        return logger
---
+++ tests/meteoroid/src/meteoroid/utilities/vault.py
@@ -0,0 +1,48 @@
+import hvac
+import os
+import pathlib
+from meteoroid import exceptions
+import logging
+
+
+VAULT_ADDR = "https://clotho.broadinstitute.org:8200"
+VAULT_TOKEN = os.environ['VAULT_TOKEN']
+
+
+def get_authenticated_vault_client(token: str) -> hvac.Client:
+    """Returns an authenticated hvac.Client instance."""
+    if pathlib.Path(token).is_file():
+        logging.info(f"Reading the Vault token from file {token}")
+        with open(token, 'r') as token_file:
+            token = token_file.read()
+
+    client = hvac.Client(url=VAULT_ADDR, token=token)
+
+    if not client.is_authenticated():
+        raise exceptions.VaultAuthException(
+            f"Failed to authenticate with Vault server {VAULT_ADDR}"
+        )
+    return client
+
+
+def read_secret(client: hvac.Client, path: str, pure_data: bool = True) -> dict:
+    """Returns the secret in a dictionary given the path in Vault."""
+    secret = client.read(path=path)
+
+    if not secret:
+        raise exceptions.InvalidVaultSecretPath(
+            f"Failed to retrieve secret from {path}"
+        )
+
+    if pure_data:
+        secret = secret['data']
+    return secret
+
+
+def list_secrets(client: hvac.Client, path: str, pure_data: bool = True) -> dict:
+    """Returns the secret list in a dictionary under the path given the path in Vault."""
+    secret_list = client.list(path=path)
+
+    if pure_data:
+        secret_list = secret_list['data']
+    return secret_list
